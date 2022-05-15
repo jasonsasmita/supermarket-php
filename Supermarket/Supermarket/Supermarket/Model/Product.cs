4c2866aaf8db534fddc35c3c3edd96c89cd67764
Jason Sasmita <105599037+jasonsasmita@users.noreply.github.com>
2022-05-15 22:59:50 +0700

    Add files via upload

---
+++ Supermarket/Supermarket/Supermarket/Model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supermarket.Model
+{
+	public class Product
+	{
+		public int ProductID { get; set; }
+
+		public string ProductName { get; set; }
+
+        public int Price{ get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
